dba232d6fc1f1f72c9fb6fad70d8b1d3e7f92476
kim-ly <SoSe2018>
2019-11-28 11:44:34 +0100

    first commit

---
+++ app/src/main/java/com/example/quiz_master/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.quiz_master
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: QuizViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        viewModel = ViewModelProviders.of(this).get(QuizViewModel::class.java)
+        initNavigation()
+    }
+
+
+    private fun initNavigation() {
+        // The NavController
+        val navController = findNavController(R.id.navHostFragment)
+
+        // Connect the navHostFragment with the ActionBar.
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+    }
+
+
+}
